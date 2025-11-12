71df2fcf3f85219bbf249607502b1dfcf26dfd17
Ubuntu <ubuntu@ip-172-31-44-32.ap-south-1.compute.internal>
2025-11-12 16:58:53 +0000

    made some changes in app.js

--- frontend/app.js
+++ frontend/app.js
@@ -25,7 +25,7 @@ app.post('/submit', async (req, res) => {
   };
 
   // ✅ Dynamically pick backend URL (local or Docker)
-  const backendURL = process.env.BACKEND_URL || 'http://localhost:5000/submit';
+  const backendURL = process.env.BACKEND_URL || '/submit';
 
   try {
     const response = await fetch(backendURL, {
